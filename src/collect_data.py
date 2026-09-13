ba1dcbdf008d9413674559923791883dd7388d1e
Olivier Doniesiewicz <olivierdoniesiewicz@gmail.com>
2026-09-13 22:27:51 +0100

    Completed full data scraper for world taekwondo competitions by making the data scraper a function and calling it in the discover events code for each competition

--- src/collect_data.py
+++ src/collect_data.py
@@ -1,67 +1,49 @@
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
 
-URL = "https://results.worldtaekwondo.org/competitions/2024-paris-olympic-games/results"
+def results_scraper(start_date, end_date, name, url):
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "lxml")
+    rows=soup.find_all("tr")
+    current_round = None
+    current_weight = None
+    fight_records = []
+    for row in rows:
+        cells = row.find_all("td")
+        row_list = []
+        winner=None
+        if "winner" in cells[3].get("class", []) :
+            winner = cells[3].get_text(strip=True)
+        else:
+            winner = cells[6].get_text(strip=True)
+        for cell in cells:
+            row_list.append(cell.get_text(strip=True))
+        #carrying forward round from previous row if doesnt have it specified
+        if (len(row_list[0])!=0):
+            current_round = row_list[0]
+        else:
+            row_list[0] = current_round
+        #carrying forward weight from previous row if doesnt have it specified
+        if len(row_list[1])!=0:
+            current_weight = row_list[1]
+        else:
+            row_list[1] = current_weight
+        #build fight dictionary
+        fight = {
+            "round" : row_list[0],
+            "weight_category" : row_list[1],
+            "fighter_a_country" : row_list[2],
+            "fighter_a" : row_list[3],
+            "score" : row_list[4],
+            "method" : row_list[5],
+            "fighter_b" : row_list[6],
+            "fighter_b_country" : row_list[7],
+            "winner" : winner,
+            "event_name" : name,
+            "start_date" : start_date,
+            "end_date" : end_date
+        }
+        fight_records.append(fight)
 
-response = requests.get(URL)
-soup = BeautifulSoup(response.text, "lxml")
-##print(response.status_code)
-##print(len(response.text))
-#print(response.text[:500])
-tables = soup.find_all("table")
-rows=soup.find_all("tr")
-#print(len(rows))
-#print(response.url)
-#print(response.history)
-#tables = soup.find_all("table")
-current_round = None
-current_weight = None
-fight_records = []
-for row in rows:
-    cells = row.find_all("td")
-    row_list = []
-    winner=None
-    #print(cells[3].get("class"), cells[6].get("class"))
-    if "winner" in cells[3].get("class", []) :
-        winner = cells[3].get_text(strip=True)
-    else:
-        winner = cells[6].get_text(strip=True)
-    for cell in cells:
-        row_list.append(cell.get_text(strip=True))
-        #print(cell.get_text(strip=True))
-    if (len(row_list[0])!=0):
-        current_round = row_list[0]
-    else:
-        row_list[0] = current_round
-    if len(row_list[1])!=0:
-        current_weight = row_list[1]
-    else:
-        row_list[1] = current_weight
-    fight = {
-        "round" : row_list[0],
-        "weight_category" : row_list[1],
-        "fighter_a_country" : row_list[2],
-        "fighter_a" : row_list[3],
-        "score" : row_list[4],
-        "method" : row_list[5],
-        "fighter_b" : row_list[6],
-        "fighter_b_country" : row_list[7],
-        "winner" : winner
-    }
-    fight_records.append(fight)
-df = pd.DataFrame(fight_records)
-#cleaned_df = 
-#print(df.shape)
-#print(df.columns)
-#print(df.head())
-#print(df.groupby("weight_category").size())
-#print(df["round"].unique())
-#print(df["weight_category"].unique())
-#print(df.isna().sum())
-invalid_winners = (df["winner"] == "")
-
-
-df=df[~invalid_winners]
-print((df == "").sum())
-df.to_csv("data/raw/paris_2024_fights_clean.csv", index=False)
+    return fight_records
--- src/discover_events.py
+++ src/discover_events.py
@@ -1,5 +1,7 @@
 import requests
 import re
+import pandas as pd
+from collect_data import results_scraper
 from datetime import datetime
 from bs4 import BeautifulSoup
 
@@ -8,39 +10,39 @@ URL = "https://results.worldtaekwondo.org/competitions"
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, "lxml")
 
-tables = soup.find_all("table")
 rows = soup.find_all("tr")
 competitions = []
 for row in rows:
     row_list = []
     cells = row.find_all("td")
     links = row.find_all("a")
-    #print(links)
     results_url = None
+    start = None
+    end = None
     for link in links:
         if link.get_text(strip = True) == "Results":
             results_url = link.get("href")
-        
-    #print(row.get_text(strip=True))
+
     for cell in cells:
-            #print(cell.get_text(strip=True))
         row_list.append(cell.get_text(strip=True))
 
     #single day date 
     if re.fullmatch(r"\d{1,2} [A-Za-z]{3} \d{4}",row_list[0]):
         start = datetime.strptime(row_list[0], "%d %b %Y")
         end = datetime.strptime(row_list[0], "%d %b %Y")
-        #print(start, end)
+
     #date range but same month
     elif re.fullmatch(r"\d{1,2} - \d{1,2} [A-Za-z]{3} \d{4}", row_list[0]):
         parts = row_list[0].split(" - ")
         parts_2 = parts[1].split(" ",1)
         start = datetime.strptime(parts[0] + " " + parts_2[1], "%d %b %Y")
         end = datetime.strptime(parts[1], "%d %b %Y")
+
     #date range but different month
     elif re.fullmatch(r"\d{1,2} [A-Za-z]{3} - \d{1,2} [A-Za-z]{3} \d{4}", row_list[0]):
         parts = row_list[0].split(" - ")
         parts_2 = parts[1].split()
+
         #check for year change
         if ((parts[0].split()[1].lower() == "dec" ) and (parts_2[1].lower() == "jan")):
             start = datetime.strptime(parts[0] + " " + str(int(parts_2[2]) -1), "%d %b %Y")
@@ -60,7 +62,7 @@ for row in rows:
     if start and end:
         if (start > end ):
             print(f"{row_list[2]} , POSSIBLE ERROR")
-        if(((year_from_name != start.year) and (year_from_name != end.year)) and year_from_name):
+        if(((year_from_name != start.year) and (year_from_name != end.year)) and year_from_name is not None):
             print(f" WARNING :POSSIBLE ERROR, {row_list[2]}, {start}, {end}")
     competition = {
     "start_date" : start,
@@ -70,9 +72,19 @@ for row in rows:
     }
 
     competitions.append(competition)
-#for i in range(len(competitions)):
-    
-    #print(competitions[i]["date"])
-#print(datetime.strptime("4 Sep 2026", "%d %b %Y"))
-#print(competitions)
-#print(competitions_data)
+all_fights = []
+failed_events = []
+for competition in competitions:
+    if competition["url"]:
+        try:
+            fights = results_scraper(competition["start_date"], competition["end_date"], competition["name"], competition["url"])
+            all_fights.extend(fights)
+        except Exception as error:
+            print(f"failed: {competition["name"]} has {error}")
+            failed_events.append(competition)
+
+df = pd.DataFrame(all_fights)
+for event in failed_events:
+    print(event["name"], "-", event["url"])
+
+df.to_csv("data/raw/world_taekwondo_fights_raw.csv", index = False)
